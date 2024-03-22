0b01bcdb61b0023b5b6d30c6409360e55fd3862b
Ron Miller <ron@cloudswarm.net>
2024-03-21 20:37:43 -0700

    Use systemd and not init

--- entrypoint.sh
+++ entrypoint.sh
@@ -177,7 +177,7 @@ if [[ ${_START_DOCKER_SERVICE} == "true" ]]; then
   echo "Starting docker service"
   _PREFIX=""
   [[ ${_RUN_AS_ROOT} != "true" ]] && _PREFIX="sudo"
-  ${_PREFIX} service docker start
+  ${_PREFIX} systemctl start docker.service
 fi
 
 # Container's command (CMD) execution as runner user
